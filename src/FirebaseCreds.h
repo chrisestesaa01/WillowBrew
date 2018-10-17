f92d108131872fa0be96f132b70483d42c7c0c0e
Chris Estes <chris.estes@willowtreeapps.com>
2018-10-17 13:32:25 -0400

    Repo init

---
+++ src/FirebaseCreds.h
@@ -0,0 +1,6 @@
+/**
+ * Creds for firebase access. These shouldn't be in the repo. 
+ **/
+
+#define FIREBASE_HOST "kegscale-eafce.firebaseio.com"
+#define FIREBASE_AUTH "bHbqDTA5DlFV9j0q4OdPRYKLUs6TGEfxPWbzOZks"
